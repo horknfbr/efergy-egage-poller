fd618277a1861d694e55fa823af81fd38957fb2e
Scott Macdonald <scottm@bed.link>
2019-04-21 16:19:23 -0400

    getting data laid out for intert into influx

--- efergy-poller/efergy-poller.py
+++ efergy-poller/efergy-poller.py
@@ -1,14 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from influxdb import InfluxDBClient
 import urllib.request
 import json
 import os
+import time
 import daemon
 
-url = os.envron['ENGAGE_API_URL']
-apiToken = os.environ['EFERGY_API_TOKEN']
+apiEndpoint = os.environ.get('ENGAGE_API_URL', '')
+apiToken = os.environ.get('EFERGY_API_TOKEN', '')
 
-with urllib.request.urlopen(url + apiToken):
-    data = json.loads(url.read().decode())
-    print(data)
+
+def getData(eUrl, eToken):
+    with urllib.request.urlopen(apiEndpoint + apiToken) as url:
+        data = json.loads(url.read().decode())
+        influxOut = []
+
+        for output in data:
+            for values in output['data']:
+                for k, v in values.items():
+                    valTime = k
+                    val = v
+            cid = output['cid']
+            sid = output['sid']
+
+            influxOut.append(
+                {"measurement": "kWm",
+                 "tags": {
+                     "cid": cid,
+                     "sid": sid,
+                     },
+                 "time": valTime,
+                 "fields": {
+                     "power": val,
+                     },
+                 }
+            )
+
+    return(influxOut)
+
+
+def main():
+    apiOut = getData(apiEndpoint, apiToken)
+    print(json.dumps(apiOut, indent=4))
+
+
+if __name__ == "__main__":
+    main()
