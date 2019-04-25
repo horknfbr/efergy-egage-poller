9c31da7312c5bd56e2dc20d460ab986b1ec74472
Scott Macdonald <scottm@bed.link>
2019-04-25 07:05:40 -0400

    adding import function

--- docker/efergy-poller/efergy-poller.py
+++ docker/efergy-poller/efergy-poller.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 from influxdb import InfluxDBClient
+import argparse
 import urllib.request
 import json
 import os
-import time
+import datetime
 import daemon
-
-apiEndpoint = os.environ.get('ENGAGE_API_URL', '')
-apiToken = os.environ.get('EFERGY_API_TOKEN', '')
+import csv
 
 
 def getData(eUrl, eToken):
-    with urllib.request.urlopen(apiEndpoint + apiToken) as url:
+    with urllib.request.urlopen(eUrl + eToken) as url:
         data = json.loads(url.read().decode())
         influxOut = []
 
         for output in data:
             for values in output['data']:
                 for k, v in values.items():
-                    valTime = k
+                    ksec = int(k) / 1000
+                    valTime = datetime.datetime.fromtimestamp(int(ksec))
                     val = v
             cid = output['cid']
             sid = output['sid']
@@ -30,7 +30,7 @@ def getData(eUrl, eToken):
                      "cid": cid,
                      "sid": sid,
                      },
-                 "time": valTime,
+                 "time": valTime.isoformat(),
                  "fields": {
                      "power": val,
                      },
@@ -40,9 +40,53 @@ def getData(eUrl, eToken):
     return(influxOut)
 
 
+def importReport(reportFile):
+    reportList = []
+    for line in csv.DictReader(open(reportFile)):
+        reportList.append(line)
+
+    return(reportList)
+
+def insetData(jsonData):
+    inclient = InfluxDBClient(
+        host='127.0.0.1',
+        port=8087,
+        username='admin',
+        password='password'
+        )
+    inclient.switch_database('efergy')
+    inclient.write_points(jsonData)
+
+
 def main():
-    apiOut = getData(apiEndpoint, apiToken)
-    print(json.dumps(apiOut, indent=4))
+    # Set options
+    parser = argparse.ArgumentParser(
+        description='Simple Python script pull from engage API \
+                    and store data in influxdb'
+    )
+    parser.add_argument(
+        "-i, --import", action="store", type=str,
+        dest="importCSV",
+        help='import csv downloaded from engage"'
+    )
+
+    cliarg = parser.parse_args()
+
+    try:
+        cliarg.importCSV
+    except NameError:
+        cliarg.importCSV = None
+
+    if cliarg.importCSV is None:
+        apiEndpoint = os.environ.get('ENGAGE_API_URL', '')
+        apiToken = os.environ.get('EFERGY_API_TOKEN', '')
+        apiOut = getData(apiEndpoint, apiToken)
+
+    else:
+        apiOut = importReport(cliarg.importCSV)
+    jsonData = json.dumps(apiOut, indent=4)
+    print(jsonData)
+    # insetData(apiOut)
 
 
 if __name__ == "__main__":
